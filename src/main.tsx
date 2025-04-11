be0d380a9789e17d658cb0cd5d97ac104fc70e7f
Fedor <osinaid@mail.ru>
2025-04-11 08:48:36 +0500

    feat: setup axios instance and context for token

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "~/context/AuthContext";
 import Home from "~/pages/Home";
 
 const queryClient = new QueryClient();
@@ -8,7 +9,9 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Home />
+      <AuthProvider>
+        <Home />
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
